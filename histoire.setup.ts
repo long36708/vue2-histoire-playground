88a96ae580f24cc3298f450ab6259694cf0c141e
zhangshilong <3131521901@qq.com>
2025-01-04 18:07:18 +0800

    init

---
+++ histoire.setup.ts
@@ -0,0 +1,11 @@
+import { defineSetupVue2 } from "@histoire/plugin-vue2";
+
+export const setupVue2 = defineSetupVue2(({ story, variant }) => {
+  // Vue plugin
+
+  return {
+    provide: {
+      key: "meow",
+    },
+  };
+});
